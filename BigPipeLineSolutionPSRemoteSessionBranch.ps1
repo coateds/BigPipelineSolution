9c15f806e065cf15e22fd6f34348656add97d40d
Dave Coate <coateds@outlook.com>
2017-12-13 07:31:19 -0800

    files from MySharedRepository

---
+++ BigPipeLineSolutionPSRemoteSessionBranch.ps1
@@ -0,0 +1,246 @@
+﻿<#
+BigPipeLineSolution PSRemote Session Branch
+
+This variant: 
+1. Opens one PSSession connection to a remote computer
+2. Stores the session object as a property in the $ComputerProperties object collection
+3. Uses this object as needed in invoke-command
+4. closes the session in one last function on the pipe:
+
+Example: Get-MyServerCollection | Test-ServerConnectionOnPipeline | Get-TimeZoneOnPipeline | Get-CultureOnPipeline | Cleanup-PSSession | ft
+
+
+#>
+
+
+<# 
+.Synopsis 
+   Gets a (filtered) list of servers from a CSV File 
+.DESCRIPTION 
+   The parameters are both optional. 
+   Leaving one blank applies no filter for that parameter. 
+.EXAMPLE 
+   Get-MyServerCollection 
+   Returns everything 
+.EXAMPLE 
+   Get-MyServerCollection -Role Web 
+   Returns all of the Web Servers 
+.EXAMPLE 
+   Get-MyServerCollection -Role SQL -Location WA 
+   Returns the SQL Servers in Washington 
+#>
+Function Get-MyServerCollection  
+    { 
+    Param 
+        ( 
+        [ValidateSet("Web", "SQL", "DC")] 
+        [string]$Role, 
+         
+        [ValidateSet("AZ", "WA")] 
+        [string]$Location 
+        ) 
+
+    # $ScriptPath = 'C:\Scripts\Book\Chap2' 
+    $ScriptPath = $PSScriptRoot
+    $ComputerNames = 'Servers.csv' 
+
+    If ($Role -ne "")  {$ModRole = $Role} 
+        Else {$ModRole = "*"} 
+    If ($Location -ne "")  {$ModLocation = $Location} 
+        Else {$ModLocation = "*"} 
+
+    Import-Csv -Path "$ScriptPath\$ComputerNames"  | 
+        Where {($_.Role -like $ModRole) -and ($_.Location -like $ModLocation)} 
+    }
+
+<#
+Simple Function to test WMI connectivity on a remote machine 
+moving the Try...Catch block into isolation helps prevent any errors on the console
+Return is the WMI OS object when sucessfully connects, Null when it does not
+#>
+Function Get-WMI_OS ($ComputerName)
+    {
+    Try {Get-Wmiobject -ComputerName $ComputerName -Class Win32_OperatingSystem -ErrorAction Stop}
+    Catch {}
+    }
+
+
+    
+
+<# 
+.Synopsis 
+    Runs availability checks on servers
+.DESCRIPTION 
+    This typically takes an imported csv file with a ComputerName Column as an imput object
+    but just about any collection of objects that exposes .ComputerName should work
+    The output is the same type of object as the input (hopefully) so that it can be piped 
+    to the next function to add another column.
+
+    I am changing this function to attempt to open a new-pssession
+    If successful, store the Session object in the computerProperties object being passed on the pipeline
+.EXAMPLE 
+    Get-MyServerCollection | Test-ServerConnectionOnPipeline | ft
+.EXAMPLE
+    $a = Foreach ($s in ('Server1','Server2','Server3')) {New-Object PSObject -Property @{'ComputerName' = $s}}
+    $a | Test-ServerConnectionOnPipeline | ft
+    Another Ad Hoc way to build an object for the pipeline. These two lines cannot
+#>
+Function Test-ServerConnectionOnPipeline
+    {
+    [CmdletBinding()]
+
+    Param
+        (
+        [parameter(
+        Mandatory=$true, 
+        ValueFromPipeline= $true)]
+        $ComputerProperties
+        )
+    
+    Begin
+        {}
+    Process
+        {
+        $ComputerProperties | Select *, Ping, WMI, PSRemote, PSSession, BootTime | %{
+            # Test Ping
+            $_.Ping = Test-Connection -ComputerName $ComputerProperties.ComputerName -Quiet -Count 1
+
+            If ($_.Ping)
+                {
+                
+                # Calling WMI in a wrapper in order to isolate the error condition if it occurs
+                $os = Get-WMI_OS -ComputerName $ComputerProperties.ComputerName
+                # $os = Get-Wmiobject -ComputerName $ComputerProperties.ComputerName -Class Win32_OperatingSystem -ErrorAction Stop
+
+                If ($os -ne $null) 
+                    {
+                    $_.BootTime = [Management.ManagementDateTimeConverter]::ToDateTime($os.LastBootUpTime) 
+                    $_.WMI = $true
+                    }
+                Else
+                    {
+                    $_.WMI = $false
+                    $_.BootTime = 'No Try'
+                    }
+
+                # Test PS Remoting
+
+                $Session = New-PSSession -ComputerName $ComputerProperties.ComputerName -ErrorAction SilentlyContinue
+
+                If ($Session -ne $null) 
+                    {$_.PSRemote = $true; $_.PSSession = $Session}
+                Else
+                    {$_.PSRemote = $false}
+                }
+            $_
+            }
+        }
+    }
+
+<# 
+.Synopsis 
+     Adds a TimeZone Column to an object
+.DESCRIPTION 
+    This typically takes an imported csv file with a ComputerName Column as an imput object
+    but just about any collection of objects that exposes .ComputerName should work
+    The output is the same type of object as the input (hopefully) so that it can be piped 
+    to the next function to add another column
+    
+    Uses PowerShell Remoting
+
+    Requires Input object with Boolean Ping and PSRemote properties. Will only try to get value 
+    if both are true
+
+    Modifying to make Invoke-Command use existing $PSSession object to prevent from having to open a new connection
+.EXAMPLE 
+    Get-MyServerCollection | Test-ServerConnectionOnPipeline | Get-TimeZoneOnPipeline | ft
+.EXAMPLE 
+    Get-MyServerCollection | Test-ServerConnectionOnPipeline | Get-TimeZoneOnPipeline | Select ComputerName,TimeZone | ft -AutoSize
+#>
+Function Get-TimeZoneOnPipeline
+    {
+    [CmdletBinding()]
+
+    Param
+        (
+        [parameter(
+        Mandatory=$true, 
+        ValueFromPipeline= $true)]
+        $ComputerProperties,
+
+        [switch]
+        $NoErrorCheck
+        )
+    
+    Begin
+        {}
+    Process
+        {
+        $ComputerProperties | Select *, TimeZone | %{
+            If ((($_.Ping) -and ($_.PSRemote)) -or ($NoErrorCheck))
+                {
+                $_.TimeZone = $null
+                
+                $sb = {(Get-ItemProperty -Path 'HKLM:\system\CurrentControlSet\control\TimeZoneInformation'`
+                     -Name TimeZoneKeyName).TimeZoneKeyName}
+                $_.TimeZone = Invoke-Command -Session $_.PSSession -ScriptBlock $sb
+                }
+            Else {$_.TimeZone = 'No Try'}
+            $_
+            }
+        }
+    }
+
+Function Get-CultureOnPipeline
+    {
+    [CmdletBinding()]
+
+    Param
+        (
+        [parameter(
+        Mandatory=$true, 
+        ValueFromPipeline= $true)]
+        $ComputerProperties,
+
+        [switch]
+        $NoErrorCheck
+        )
+    
+    Begin
+        {}
+    Process
+        {
+        $ComputerProperties | Select *, Culture | %{
+            If ((($_.Ping) -and ($_.PSRemote)) -or ($NoErrorCheck))
+                {
+                $_.Culture = $null
+                
+                $sb = {(Get-Culture).Name}
+                $_.Culture = Invoke-Command -Session $_.PSSession -ScriptBlock $sb
+                }
+            Else {$_.Culture = 'No Try'}
+            $_
+            }
+        }
+    }
+
+Function Cleanup-PSSession
+    {
+    [CmdletBinding()]
+
+    Param
+        (
+        [parameter(
+        Mandatory=$true, 
+        ValueFromPipeline= $true)]
+        $ComputerProperties,
+
+        [switch]
+        $NoErrorCheck
+        )
+    
+    Begin
+        {}
+    Process
+        {Remove-PSSession -Session $_.PSSession; $_}
+    }
---
+++ WriteRegWithUndo.ps1
@@ -0,0 +1,154 @@
+﻿Function Get-RegItemDetails
+    {
+    Param($Path)
+
+    $regKey=Get-Item $Path
+    $regkey.GetvalueNames() | % {New-Object PSObject -Property @{'Name' = $_; 'Type' = $regkey.getvaluekind($_); 'Value' = $regkey.getvalue($_)}  }
+    }
+
+Function Get-RegItemType
+    {
+    Param($Path,$Name)
+
+    $regKey=Get-Item $Path
+    ($regkey.GetvalueNames() | % {New-Object PSObject -Property @{'Name' = $_; 'Type' = $regkey.getvaluekind($_)}  } | Where Name -eq $Name).Type
+    }
+
+<#
+$RegValue = Get-RegItemDetails -Path HKLM:\software\mysoftware | Where Name -eq MyNewItem5
+If ($RegValue -eq $null) {"Not Found"}
+
+
+$Type = Get-RegItemType -Path HKLM:\software\mysoftware -Name MyNewItem5
+If ($Type -eq $null) {$Type = "Not Found"}
+$Type
+#>
+
+<#
+.EXAMPLE
+    ('Server1','Server2') | Get-ServerObjectCollection | Test-ServerConnectionOnPipeline | 
+    Set-RegItemWithUndo -Path HKLM:\software\mysoftware -Name MyNewItem -Type String -Value Fubart | 
+    Select ComputerName,Path,Name,OriginalType,OriginalValue,Result  | ft
+
+    Use: Invoke-Command -ScriptBlock {New-Item -Path HKLM:\software\MySoftware} -ComputerName (Get-MyServerCollection).ComputerName
+    From the console to create the Reg Keys on all computers
+.EXAMPLE
+    Get-MyServerCollection | Test-ServerConnectionOnPipeline | 
+    Set-RegItemWithUndo -Path HKLM:\software\mysoftware -Name MyNewItem -Type String -Value Mickey | 
+    Select ComputerName,Path,Name,OriginalType,OriginalValue,Result  | Export-Csv .\RegMyNewItem.csv
+#>
+Function Set-RegItemWithUndo
+    {
+    [CmdletBinding()]
+
+    Param
+        (
+        [parameter(
+        Mandatory=$true, 
+        ValueFromPipeline= $true)]
+        $ComputerProperties,
+
+        [switch]
+        $NoErrorCheck,
+
+        $Path,
+        
+        $Name,
+        
+        [ValidateSet('String', 'ExpandString', 'Binary', 'DWord', 'MultiString', 'QWord')] 
+        $Type,
+        
+        $Value
+        )
+    
+    Begin
+        {}
+    Process
+        {
+        $ComputerProperties | Select *, Path, Name, OriginalType, OriginalValue, Result | %{
+            If ((($_.Ping) -and ($_.PSRemote)) -or ($NoErrorCheck))
+                {
+                # Write the Path and Name for the record
+                $_.Path = $Path
+                $_.Name = $Name
+
+                $sb = 
+                    {
+                    $Path = $Args[0]
+                    $Name = $Args[1]
+                    
+                    $regkey1 = (Get-Item $Path)
+                    $regkey1.GetvalueNames() | 
+                        % {New-Object PSObject -Property @{'Name' = $_; 'Type' = $regkey1.getvaluekind($_); 'Value' = $regkey1.getvalue($_)}} |
+                        Where Name -eq $Name
+
+                    # At this point, I need to add a success/fail column to the output object
+                    }
+                
+                # Run the Script Block
+                $RegValue = Invoke-Command -ComputerName $_.ComputerName -ScriptBlock $sb -ArgumentList $Path, $Name -ErrorAction SilentlyContinue
+                
+                $_.OriginalValue = $RegValue.Value
+                $_.OriginalType = $RegValue.Type
+                If ($_.OriginalValue -eq $null) {$_.OriginalValue = 'Not Found'}
+
+                # Make another Invoke-Command call here with return of Success or Failure
+                $sb1 = 
+                    {
+                    $Path = $Args[0]
+                    $Name = $Args[1]
+                    $Type = $Args[2]
+                    $Value = $Args[3]
+
+                    Try
+                        {Set-ItemProperty -Path $Path -Name $Name -Type $Type -Value $Value -ErrorAction Stop}
+                    Catch
+                        {'Error'}
+                    }
+
+                $_.Result = Invoke-Command -ComputerName $_.ComputerName -ScriptBlock $sb1 -ArgumentList $Path, $Name, $Type, $Value
+                If ($_.Result -eq $null) {$_.Result = 'Success'}
+
+                # Now write the whole mess out to the output
+                $_
+                }
+            }
+        }
+    }
+
+Function Set-UndoRegSettingFromCsv
+    {
+    [CmdletBinding()]
+
+    Param
+        (
+        [parameter(
+        Mandatory=$true, 
+        ValueFromPipeline= $true)]
+        $ComputerProperties,
+
+        [switch]
+        $NoErrorCheck
+        )
+
+    Begin
+        {}
+    Process
+        {
+        $ComputerProperties | Select *, UndoResult | %{
+            If ((($_.Ping) -and ($_.PSRemote)) -or ($NoErrorCheck))
+                {
+                'For ' + $_.ComputerName + ' -'
+                '    Depending on Result: ' + $_.Result
+                '    Reset ' + $_.Path + ' - ' + $_.Name
+                '    To ' + $_.OriginalType + ': ' + $_.OriginalValue
+                }
+            # $_
+            }
+        }
+    }
+
+    # $Path = 'HKLM:\software\mysoftware'
+    # $Name =  'MyNewItem'
+    # $regkey1 = (Get-Item $Path)
+    # $regkey1.GetvalueNames() | % {New-Object PSObject -Property @{'Name' = $_; 'Type' = $regkey1.getvaluekind($_); 'Value' = $regkey1.getvalue($_)}  }
